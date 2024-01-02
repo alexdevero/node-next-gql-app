d217f34cd71a6591a349c6431881a1496ae30f8f
Alex Devero <deveroalex@gmail.com>
2024-01-02 10:50:56 +0100

    BE: Fix db connection outside docker

--- backend/mikro-orm.config.ts
+++ backend/mikro-orm.config.ts
@@ -3,11 +3,17 @@ import { TsMorphMetadataProvider } from '@mikro-orm/reflection'
 
 import { User } from './src/entities/user'
 
+// Used as a hack to distinguish between running in docker and locally
+// If DATABASE_URL is set, we are running in docker
+const DB_URL = process.env.DATABASE_URL
+
 const config: Options = {
   entities: [User],
   dbName: 'postgres',
   type: 'postgresql',
-  clientUrl: process.env.DATABASE_URL,
+  ...(DB_URL
+    ? { clientUrl: process.env.DATABASE_URL }
+    : { host: 'localhost', port: 5432, password: 'postgres', user: 'postgres' }),
   debug: true,
   metadataProvider: TsMorphMetadataProvider,
 }
